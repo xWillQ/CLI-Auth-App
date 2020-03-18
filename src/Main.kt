ca45aa4495f8572e45eeb875ad920eb7e74c0db2
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 18:51:55 +0700

    Add files via upload

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,28 @@
 import Handler
+import InitObjects.*
 import models.User
 import models.Session
 import models.Resource
-import services.AuthenService
+import services.AuthenticationService
 import services.AuthorizeService
 import services.HelpService
 import ExitCodes.*
 import kotlin.system.exitProcess
 
 
-fun main() {
-    print("Hello World")
+fun main(args: Array<String>) {
+    val users = initUsers()
+    val recources = initResources()
+    val Handler = Handler(args)
+    val help = HelpService()
+    val authenService = AuthenService(users, recources)
+    if (Handler.authenNeeded()) {
+        login = Handler.getLogin()
+        password = Handler.getPassword()
+        authenService.authentication(login, password)
+    } else {
+        help.printHelp()
+        exitProcess(SuccessCode.code)
+    }
 }
+
