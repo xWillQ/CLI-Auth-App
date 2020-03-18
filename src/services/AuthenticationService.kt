8546ad766a734432852c3a54e8dec8b4e21535d5
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 19:57:55 +0700

    4

--- src/services/AuthenticationService.kt
+++ src/services/AuthenticationService.kt
@@ -1,17 +1,33 @@
 package services
 
+import ExitCodes.*
 import java.math.BigInteger
 import java.security.MessageDigest
 import models.User
 import models.Resource
+import kotlin.system.exitProcess
 
 class AuthenticationService(val users: List<User>, val resource: List<Resource>) {
-    fun authentication(login: String, password: String) {
+    fun authentication(login: String, password: String): User {
+        if (!CheckLoginValidity(login)) {
+            exitProcess(InvalidLoginFormat.code)
+        }
+        if (CheckLoginPresenceInBase(login, users) == null) {
+            exitProcess(UnknownLogin.code)
+        }
+        val nowHash = GetPasswordHash(password)
+        val user = GetUser(login, users)
+        val baseHash = GetPasswordHashInBase(user)
+        if (!CheckPasswordsHashs(baseHash, nowHash)) {
+            exitProcess(InvalidPassword.code)
+        }
 
+        return user
     }
 
     fun CheckLoginValidity(login: String): Boolean = login.matches(Regex("[a-z]{1,10}"))
     fun CheckLoginPresenceInBase(login: String, users: List<User>): User? = users.find { it.login == login }
+    fun GetUser(login: String, users: List<User>): User = users.first { it.login == login }
     fun GetPasswordHash(password: String): String = password.md5()
     fun GetPasswordHashInBase(user: User): String = user.hash
     fun CheckPasswordsHashs(baseHash: String, nowHash: String): Boolean = baseHash == nowHash
