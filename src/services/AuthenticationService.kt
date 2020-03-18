9a201a8fb9dbb81fdc2503317eb5f15b44129f3b
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 19:28:55 +0700

    4.5

--- src/services/AuthenticationService.kt
+++ src/services/AuthenticationService.kt
@@ -1,5 +1,7 @@
 package services
 
+import java.math.BigInteger
+import java.security.MessageDigest
 import models.User
 import models.Resource
 
@@ -7,11 +9,16 @@ class AuthenticationService(val users: List<User>, val resource: List<Resource>)
     fun authentication(login: String, password: String) {
 
     }
+
     fun CheckLoginValidity(login: String): Boolean = login.matches(Regex("[a-z]{1,10}"))
-    fun CheckLoginPresenceInBase(login: String, users: List<User>): User? = users.find {it.login == login}
+    fun CheckLoginPresenceInBase(login: String, users: List<User>): User? = users.find { it.login == login }
+    fun GetPasswordHash(password: String): String = password.md5()
 
-
+    fun String.md5(): String {
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
     }
+}
 
 
 }
