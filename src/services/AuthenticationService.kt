1b7e67ae2b56a70ad8b55a1d2f908f76df1fe3c9
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 19:09:29 +0700

    4.2

--- src/services/AuthenticationService.kt
+++ src/services/AuthenticationService.kt
@@ -1,10 +1,3 @@
-package services
-
-import models.User
-import models.Resource
-
-class AuthenticationService(val users: List<User>, val resource: List<Resource>) {
-    fun authentication(login: String, password: String) {
-
-    }
+class Resource {
+    var name
 }
