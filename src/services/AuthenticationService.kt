1f464260b1c2bf0560ed326f2ddc0a06be3036ff
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 19:13:37 +0700

    4.3

--- src/services/AuthenticationService.kt
+++ src/services/AuthenticationService.kt
@@ -1,3 +1,17 @@
-class Resource {
-    var name
+package services
+
+import models.User
+import models.Resource
+
+class AuthenticationService(val users: List<User>, val resource: List<Resource>) {
+    fun authentication(login: String, password: String) {
+
+    }
+    fun CheckLoginValidity(login: String): Boolean = login.matches(Regex("[a-z]{1,10}"))
+    fun CheckLoginPresenceInBase(login: String, users: List<User>): User? = users.find {it.login == login}
+
+
+    }
+
+
 }
