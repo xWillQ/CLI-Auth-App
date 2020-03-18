58fb762b4cbf9b86beb9537ab122e74cad824f30
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 20:37:36 +0700

    Откат коммита

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,40 @@
 import Handler
+import InitObjects.*
 import models.User
 import models.Session
 import models.Resource
-import services.AuthenService
+import services.AuthenticationService
 import services.AuthorizeService
 import services.HelpService
 import ExitCodes.*
+import javax.security.sasl.AuthorizeCallback
 import kotlin.system.exitProcess
 
 
-fun main() {
-    exitProgramm(SucessCode)
+fun main(args: Array<String>) {
+    val users = initUsers()
+    val user: User?
+    val recources = initResources()
+    val handler = Handler(args)
+    val help = HelpService()
+    val authenService = AuthenticationService(users, recources)
+    if (handler.authenNeeded()) {
+        val login = handler.getLogin()
+        val password = handler.getPassword()
+        user = authenService.authentication(login, password)
+
+    } else {
+        help.printHelp()
+        exitProcess(SuccessCode.code)
+    }
+    val authorizeService = AuthorizeService()
+    if (handler.authorizeNeeded()) {
+        val role = handler.getRole()
+        val resource = handler.getResource()
+        val AuthorizeSuccess = authorizeService.authorization(user, role, resource)
+    } else {
+        help.printHelp()
+        exitProcess(SuccessCode.code)
+    }
 }
+
