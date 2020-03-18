24f13466606763dddba6cf2c0c585239e5ccddbd
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-18 16:44:49 +0700

    Create Main.kt

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+import Handler
+import models.User
+import models.Session
+import models.Resorce
+import services.AuthenService
+import services.AuthorizeService
+import services.HelpService
+import kotlin.system.exitProcess
+
+
+fun main() {
+    print("Hello World")
+}
