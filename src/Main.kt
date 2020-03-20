6d24faf9992b52ceec5716b945e664a5aecce943
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-20 13:44:52 +0700

    Исправил нейминг

--- src/Main.kt
+++ src/Main.kt
@@ -1,43 +1,36 @@
-import Handler
 import InitObjects.*
 import ExitCodes.*
-import models.*
 import services.*
 import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>) {
     val users = initUsers()
-    val user: User?
-    val AuthorizeSuccess: Boolean?
-    val recources = initResources()
+    val resources = initResources()
     val handler = Handler(args)
-    val authenService = AuthenticationService(users, recources)
-    if (handler.authenNeeded()) {
-        val login = handler.getLogin()
-        val password = handler.getPassword()
-        user = authenService.authentication(login, password)
-
-    } else {
-        printHelp()
-        exitProcess(SuccessCode.code)
-    }
-    val authorizeService = AuthorizationService(recources)
-    if (handler.authorizeNeeded()) {
-        val role = handler.getRole()
-        val resourceName = handler.getResource()
-        AuthorizeSuccess = authorizeService.authorization(user, role, resourceName)
-    } else {
-        printHelp()
-        exitProcess(SuccessCode.code)
-
-    }
-    if (!AuthorizeSuccess) {
-        exitProcess(NoAccess.code)
-    }
+    val authService = AuthenticationService(users)
+    val authorizeService = AuthorizationService(resources)
     val accountService = AccountingService()
-    if (Handler.accountingNeeded()) {
-        accountService.accounting()
-
+    if (!handler.authenNeeded()) {
+        terminate(true, HelpCode.code)
+    } else {
+        val user = authService.authentication(handler.login, handler.password)
+        if (!handler.authorizeNeeded()) {
+            terminate(true, HelpCode.code)
+        } else {
+            val haveAccess = authorizeService.authorization(user, handler.role, handler.resource)
+            when {
+                (!haveAccess) -> terminate(false, NoAccess.code)
+                (Handler.accountingNeeded()) -> accountService.accounting()
+                else -> terminate(false, SuccessCode.code)
+            }
+        }
     }
+}
+
+fun terminate(printHelp: Boolean, errorCode: Int) {
+    if (printHelp) {
+        printHelp()
+    }
+    exitProcess(errorCode)
 }
