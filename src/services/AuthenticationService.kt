6fddddbbde0d654439059b3165f89916ee114ec4
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-20 13:40:50 +0700

    Исправил Code Style

--- src/services/AuthenticationService.kt
+++ src/services/AuthenticationService.kt
@@ -4,40 +4,38 @@ import ExitCodes.*
 import java.math.BigInteger
 import java.security.MessageDigest
 import models.User
-import models.Resource
 import kotlin.system.exitProcess
 
-class AuthenticationService(val users: List<User>, val resource: List<Resource>) {
+class AuthenticationService(private val users: List<User>) {
     fun authentication(login: String, password: String): User {
-        if (!CheckLoginValidity(login)) {
+        if (!checkLoginValidity(login)) {
             exitProcess(InvalidLoginFormat.code)
         }
-        if (CheckLoginPresenceInBase(login, users) == null) {
+        if (checkLoginPresenceInBase(login, users) == null) {
             exitProcess(UnknownLogin.code)
         }
-        val nowHash = GetPasswordHash(password)
-        val user = GetUser(login, users)
-        val baseHash = GetPasswordHashInBase(user)
-        if (!CheckPasswordsHashs(baseHash, nowHash)) {
+        val nowHash = getPasswordHash(password)
+        val user = getUser(login, users)
+        val baseHash = getPasswordHashInBase(user)
+        if (!checkPasswordsHashs(baseHash, nowHash)) {
             exitProcess(InvalidPassword.code)
         }
 
         return user
     }
 
-    fun CheckLoginValidity(login: String): Boolean = login.matches(Regex("[a-z]{1,10}"))
-    fun CheckLoginPresenceInBase(login: String, users: List<User>): User? = users.find { it.login == login }
-    fun GetUser(login: String, users: List<User>): User = users.first { it.login == login }
-    fun GetPasswordHash(password: String): String = password.md5()
-    fun GetPasswordHashInBase(user: User): String = user.hash
-    fun CheckPasswordsHashs(baseHash: String, nowHash: String): Boolean = baseHash == nowHash
+    private fun checkLoginValidity(login: String): Boolean = login.matches(Regex("[a-z]{1,10}"))
+    private fun checkLoginPresenceInBase(login: String, users: List<User>): User? = users.find { it.login == login }
+    private fun getUser(login: String, users: List<User>): User = users.first { it.login == login }
+    private fun getPasswordHash(password: String): String = password.md5()
+    private fun getPasswordHashInBase(user: User): String = user.hash
+    private fun checkPasswordsHashs(baseHash: String, nowHash: String): Boolean = baseHash == nowHash
 
 
-    fun String.md5(): String {
+    private fun String.md5(): String {
         val md = MessageDigest.getInstance("MD5")
         return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
     }
 }
 
 
-}
