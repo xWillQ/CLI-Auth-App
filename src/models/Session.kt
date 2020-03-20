44d657ff7b68acf85e6d4257679a631798daeddf
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-20 14:40:29 +0700

    Поправил CodeStyle

--- src/models/Session.kt
+++ src/models/Session.kt
@@ -1,33 +1,32 @@
 package models
+
 import ExitCodes.*
 
 import java.time.LocalDate
 import kotlin.system.exitProcess
 
-class Session(val dateStart: String, val dateEnd: String, val vol: String, val res: Resource) {
-    var formattedDateStart : LocalDate = LocalDate.now()
+class Session(dateStart: String, dateEnd: String, vol: String, val res: Resource) {
+    var formattedDateStart: LocalDate = LocalDate.now()
         private set
-    var formattedDateEnd : LocalDate = LocalDate.now()
+    var formattedDateEnd: LocalDate = LocalDate.now()
         private set
     var volume: Int = 0
         private set
+
     init {
         try {
             formattedDateStart = LocalDate.parse(dateStart)
             formattedDateEnd = LocalDate.parse(dateEnd)
+        } catch (e: java.time.format.DateTimeParseException) {
+            exitProcess(IncorrectActivity.code)
         }
-        catch (e: java.time.format.DateTimeParseException) {
-            exitProcess(ExitCodes.IncorrectActivity.code)
-        }
-        if (formattedDateStart.isAfter(formattedDateEnd))
-        {
+        if (formattedDateStart.isAfter(formattedDateEnd)) {
             exitProcess(IncorrectActivity.code)
         }
         try {
             volume = vol.toInt()
-        }
-        catch (e: NumberFormatException) {
-            exitProcess(ExitCodes.IncorrectActivity.code)
+        } catch (e: NumberFormatException) {
+            exitProcess(IncorrectActivity.code)
         }
     }
 }
