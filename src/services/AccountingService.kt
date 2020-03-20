9a070df665cf6bc10418ff68b0dd00462166b5f3
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-20 14:45:19 +0700

    Update AccountingService.kt

--- src/services/AccountingService.kt
+++ src/services/AccountingService.kt
@@ -3,7 +3,7 @@ package services
 import models.*
 
 class AccountingService {
-    fun accounting(dateStart: String, dateEnd: String, volume: String, resource: Resource) {
+    fun accounting(dateStart: String, dateEnd: String, volume: String, resource: String) {
         val session = Session(dateStart, dateEnd, volume, resource)
     }
 }
