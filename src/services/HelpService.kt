37c39d51a586f594675fb765a1e88d3358ef756a
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-20 20:55:21 +0700

    Add files via upload

--- src/services/HelpService.kt
+++ src/services/HelpService.kt
@@ -2,8 +2,8 @@ package services
 
 private var helpMessage = """
         POSSIBLE COMMANDS
-        -h — prints out help
-        -login <str> -pass <str> — authentication with given credits
+        -h - prints out help
+        -login <str> -pass <str> - authentication with given credits
         -login <str> -pass <str> -res <str> -role <str> - authorization to the source with given credits and a role
         -ds <YYYY-MM-DD> -de <YYYY-MM-DD> -vol <int> - adds a session where -ds is a start date, -de is an end date and -vol is amount of consumed data
         NOTE that all arguments can be passed in any order
