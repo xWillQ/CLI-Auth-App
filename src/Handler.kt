d4cbc77ca14b2aca33cea718777c30f1248cc70c
ArtBekk <arturbekk@gmail.com>
2020-03-20 10:38:46 +0700

    Пункт 3.3.1

--- src/Handler.kt
+++ src/Handler.kt
@@ -1,11 +1,15 @@
-import services.helpMessage
+import services.*
 import kotlin.system.exitProcess
 import ExitCodes.*
 
 class Handler(args: Array<String>) {
     init {
         if (args.isEmpty()) {
-            print(helpMessage)
+            printHelp()
+            exitProcess(HelpCode.code)
+        }
+        if (args[0] == "-h") {
+            printHelp()
             exitProcess(HelpCode.code)
         }
     }
