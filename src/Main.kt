ccdac5189b77de0157e57ac519a3eab5766e4e2a
PetrSed <45389741+PetrSed@users.noreply.github.com>
2020-03-20 13:27:25 +0700

    переписал Main

--- src/Main.kt
+++ src/Main.kt
@@ -8,7 +8,44 @@ import kotlin.system.exitProcess
 fun main(args: Array<String>) {
     val users = initUsers()
     val user: User?
-    val AuthorizeSuccess: Boolean?
+    val AuthorizeSuccess: Boolean?import InitObjects.*
+import ExitCodes.*
+import models.*
+import services.*
+import kotlin.system.exitProcess
+
+
+fun main(args: Array<String>) {
+    val users = initUsers()
+    val recources = initResources()
+    val handler = Handler(args)
+    val authenService = AuthenticationService(users, recources)
+    val authorizeService = AuthorizationService(recources)
+    val accountService = AccountingService()
+    if (!handler.authenNeeded()) {
+        terminate(true, HelpCode.code)
+    } else {
+        val user = authenService.authentication(handler.login, handler.password)
+        if (!handler.authorizeNeeded()) {
+            terminate(true, HelpCode.code)
+        } else {
+            val HaveAcces = authorizeService.authorization(user, handler.role, handler.resource)
+            when {
+                (!HaveAcces) -> terminate(false, NoAccess.code)
+                (Handler.accountingNeeded()) -> accountService.accounting()
+                else -> terminate(false, SuccessCode.code)
+            }
+        }
+    }
+}
+
+fun terminate(printHelp: Boolean, errorCode: Int) {
+    if (printHelp) {
+        printHelp()
+    }
+    exitProcess(errorCode)
+}
+
     val recources = initResources()
     val handler = Handler(args)
     val authenService = AuthenticationService(users, recources)
